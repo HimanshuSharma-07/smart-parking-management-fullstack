af29b35b429ffa2bc336700a7219bed01b75ce76
HimanshuSharma-07 <sharmahimanshu82194.com>
2026-03-01 23:38:05 +0530

    refactor: enhance server startup logic with error handling for MongoDB connection

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,10 +2,18 @@ import dotenv from "dotenv"
 dotenv.config({
     path: '.env'
 })
-
 import connectDB from "./db/index"
+import app from "./app"
 
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`⚙️   Server is listening at ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!!", err)
+})
 
