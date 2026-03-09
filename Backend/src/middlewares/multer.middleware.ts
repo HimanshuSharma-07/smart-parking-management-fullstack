72e0b60125be5c6442c955262d21a686c6f3f29d
HimanshuSharma-07 <sharmahimanshu82194.com>
2026-03-09 23:42:19 +0530

    feat: add multer middleware for file uploads with disk storage configuration

---
+++ Backend/src/middlewares/multer.middleware.ts
@@ -0,0 +1,16 @@
+import multer from "multer"
+import { Request } from "express"
+
+const storage = multer.diskStorage({
+  destination: function (req: Request, file: Express.Multer.File, cb: Function) {
+    cb(null, "./public/temp")
+  },
+  filename: function (req: Request, file: Express.Multer.File, cb: Function) {
+    
+    cb(null, file.originalname)
+  }
+})
+
+export const upload = multer({ 
+    storage, 
+})
