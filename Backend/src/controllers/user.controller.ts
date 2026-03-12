66c6ecc89dca04b97afd22aeb49c2579bc094c7e
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-03-13 02:17:58 +0530

    feat: add logout functionality with token invalidation and cookie clearing

--- Backend/src/controllers/user.controller.ts
+++ Backend/src/controllers/user.controller.ts
@@ -6,6 +6,14 @@ import { uploadOnCloudinary } from "../utils/cloudinary"
 import { ApiResponse } from "../utils/ApiResponse"
 import { Types } from "mongoose"
 
+declare global {
+    namespace Express {
+        interface Request {
+            user?: any
+        }
+    }
+}
+
 const generateAccessAndRefreshToken = async (userId: string | Types.ObjectId) => {
     try {
         const user = await User.findById(userId)
@@ -134,10 +142,34 @@ const loginUser = asyncHandler( async (req: Request, res: Response) => {
 })
 
 const logoutUser = asyncHandler (async ( req: Request, res: Response) => {
-    
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+     }
+
+     return res
+     .status(200)
+     .clearCookie("accessToken", options)
+     .clearCookie("refreshToken", options)
+     .json(new ApiResponse(200, {}, "User logged Out Successfully"))
+
 })
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
