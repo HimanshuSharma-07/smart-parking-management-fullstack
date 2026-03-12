10ee75cc324fc2220727343b3cfff5e51989d6af
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-03-13 00:00:51 +0530

    feat: add login route to user routes for user authentication

--- Backend/src/routes/user.routes.ts
+++ Backend/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller";
+import { registerUser, loginUser } from "../controllers/user.controller";
 import { upload } from "../middlewares/multer.middleware";
 
 const router = Router()
@@ -14,7 +14,7 @@ router.route("/register").post(
     ]),
     registerUser
 )
-// router.route("/login").post(login)
+router.route("/login").post(loginUser)
 
 
 export default router
