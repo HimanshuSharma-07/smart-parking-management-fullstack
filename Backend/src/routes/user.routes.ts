7e089ed63a5be0d49c54e73bfb18b49ef9eccc8f
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-03-14 12:04:01 +0530

    create user update Details and get user details routes

--- Backend/src/routes/user.routes.ts
+++ Backend/src/routes/user.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, updateAccountDetails, getCurrentUser } from "../controllers/user.controller";
 import { upload } from "../middlewares/multer.middleware";
 import { verifyJWT } from "../middlewares/auth.middleware";
+import multer from "multer";
 
 const router = Router()
 
@@ -21,6 +22,8 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT ,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/update-details").post(verifyJWT, updateAccountDetails)
+router.route("/get-user").get(verifyJWT, getCurrentUser)
 
 
 
