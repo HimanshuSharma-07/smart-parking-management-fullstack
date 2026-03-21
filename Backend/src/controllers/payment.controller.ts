72d70d586d8a668f1047bde6ea6d19f27cfa1dae
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-03-21 22:49:52 +0530

    write create payyment and verfiy online paymet

--- Backend/src/controllers/payment.controller.ts
+++ Backend/src/controllers/payment.controller.ts
@@ -1,8 +1,10 @@
 import { Booking } from "../models/bookig.model";
 import { Payment } from "../models/payment.model";
 import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 import { Request, Response } from "express";
+
 
 
 const createPayment = asyncHandler ( async (req: Request, res: Response) => {
@@ -22,6 +24,52 @@ const createPayment = asyncHandler ( async (req: Request, res: Response) => {
         throw new ApiError(400, "Payment already existed for this booking")
     }
 
-    
+    const payment = await Payment.create({
+        bookingId,
+        
+        paymentMethod,
+        paymentStatus: paymentMethod === "cash" ? "paid" : "pending",
+        paidAt: paymentMethod === "cash" ? new Date() : undefined
+
+    })
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201, payment, "Payment created Successfully")
+    )
 
 })
+
+const verifyPayment = asyncHandler( async (req: Request, res: Response) =>{
+
+    const { paymentId } = req.params
+
+    const payment = await Payment.findById(paymentId)
+
+    if (!payment) {
+        throw new ApiError(404, "Payment not found")
+    }
+
+    if (payment.paymentStatus == "paid") {
+        throw new ApiError(400, "Already paid")
+    }
+
+    payment.paymentStatus = "paid"
+    payment.paidAt = new Date()
+
+    await payment.save()
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, payment,"Payment verified Successfully")
+    )
+
+})
+
+export {
+    createPayment,
+    verifyPayment
+
+}
